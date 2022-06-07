5080b76763bd24715e6980dac78bb08b0ed0d52c
glebbash <glebbash@gmail.com>
2022-06-07 16:31:47 +0000

    feat: migrate test to deno

--- src/mod.ts
+++ src/mod.ts
@@ -1,1 +1,1 @@
-export * from ',/mod.ts';
+export * from "./panic.ts";
--- src/panic.test.ts
+++ src/panic.test.ts
@@ -1,12 +1,23 @@
-import { panic } from './panic';
+import { panic } from "./panic.ts";
+import { expect } from "https://deno.land/x/expect@v0.2.9/mod.ts";
 
-describe('panic', () => {
-  it('throws error with message', () => {
-    const message = 'oops';
-    expect(() => panic(message)).toThrow(new Error(message));
-  });
-  it('throws error with message', () => {
-    const error = new Error('oops');
-    expect(() => panic(error)).toThrow(error);
-  });
+Deno.test("it throws error with provided message", () => {
+  const message = "oops";
+  const err = returnThrown(() => panic(message));
+  expect(err instanceof Error).toBe(true);
+  expect((err as Error).message).toBe(message);
 });
+
+Deno.test("it throws custom errors", () => {
+  const error = new Error("oops");
+  const err = returnThrown(() => panic(error));
+  expect(err).toBe(error);
+});
+
+function returnThrown(fn: () => void): unknown {
+  try {
+    fn();
+  } catch (error) {
+    return error;
+  }
+}
